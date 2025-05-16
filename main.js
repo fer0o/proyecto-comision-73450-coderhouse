f382a0798912f9d61d1715e2d05bb21c693ab811
fer0o <fer0osennin@gmail.com>
2025-05-16 17:16:25 -0600

    agregado un mockData con informacion que maneja el dom en el main

--- main.js
+++ main.js
@@ -1,115 +1,130 @@
+// main.js
+
+// imports
+import { presionInfo } from "./utils/presionInfo/presionMockData.js";
+
 document.addEventListener("DOMContentLoaded", () => {
-    const vistas = {
-      menu: document.getElementById("vistaMenu"),
-      medicion: document.getElementById("vistaMedicion"),
-      registro: document.getElementById("vistaRegistro"),
-      ayuda: document.getElementById("vistaAyuda"),
-      presion: document.getElementById("vistaPresionArterial"),
-    };
-  
-    const mostrarVista = (vistaSeleccionada) => {
-      for (let vista in vistas) {
-        vistas[vista].classList.add("hidden");
-      }
-      vistas[vistaSeleccionada].classList.remove("hidden");
-    };
-  
-    // Navegación entre vistas
-    document.getElementById("btnMedicion").addEventListener("click", () => mostrarVista("medicion"));
-    document.getElementById("btnRegistro").addEventListener("click", () => mostrarVista("registro"));
-    document.getElementById("btnAyuda").addEventListener("click", () => mostrarVista("ayuda"));
-    document.getElementById("btnVolverMenuDesdeMedicion").addEventListener("click", () => mostrarVista("menu"));
-    document.getElementById("btnVolverMenuDesdeRegistro").addEventListener("click", () => mostrarVista("menu"));
-    document.getElementById("btnVolverMenuDesdeAyuda").addEventListener("click", () => mostrarVista("menu"));
-    document.getElementById("btnVolverDesdePresion").addEventListener("click", () => mostrarVista("medicion"));
-  
-    // Navegación de botones en medición
-    const btnPresionArterial = document.getElementById("btnPresionArterial");
-    const btnRitmoCardiaco = document.getElementById("btnRitmoCardiaco");
-  
-    if (btnPresionArterial) {
-      btnPresionArterial.addEventListener("click", () => {
-        mostrarVista("presion");
+  const vistas = {
+    menu: document.getElementById("vistaMenu"),
+    medicion: document.getElementById("vistaMedicion"),
+    registro: document.getElementById("vistaRegistro"),
+    ayuda: document.getElementById("vistaAyuda"),
+    presion: document.getElementById("vistaPresionArterial"),
+  };
+
+  const mostrarVista = (vistaSeleccionada) => {
+    for (let vista in vistas) {
+      vistas[vista].classList.add("hidden");
+    }
+    vistas[vistaSeleccionada].classList.remove("hidden");
+  };
+
+  // Navegación entre vistas
+  document.getElementById("btnMedicion").addEventListener("click", () => mostrarVista("medicion"));
+  document.getElementById("btnRegistro").addEventListener("click", () => mostrarVista("registro"));
+  document.getElementById("btnAyuda").addEventListener("click", () => mostrarVista("ayuda"));
+  document.getElementById("btnVolverMenuDesdeMedicion").addEventListener("click", () => mostrarVista("menu"));
+  document.getElementById("btnVolverMenuDesdeRegistro").addEventListener("click", () => mostrarVista("menu"));
+  document.getElementById("btnVolverMenuDesdeAyuda").addEventListener("click", () => mostrarVista("menu"));
+  document.getElementById("btnVolverDesdePresion").addEventListener("click", () => mostrarVista("medicion"));
+
+  // Navegación de botones en medición
+  const btnPresionArterial = document.getElementById("btnPresionArterial");
+  const btnRitmoCardiaco = document.getElementById("btnRitmoCardiaco");
+
+  if (btnPresionArterial) {
+    btnPresionArterial.addEventListener("click", () => {
+      mostrarVista("presion");
+      cargarInformacionPresion(); // ✅ Cargar información de presión al mostrar la vista
+    });
+  }
+
+  if (btnRitmoCardiaco) {
+    btnRitmoCardiaco.addEventListener("click", () => {
+      console.log("Ritmo Cardíaco seleccionado");
+    });
+  }
+
+  // Capturamos los datos del formulario de presión arterial
+  const formPresionArterial = document.getElementById("formPresionArterial");
+  if (formPresionArterial) {
+    formPresionArterial.addEventListener("submit", (event) => {
+      event.preventDefault();
+      calcularPresion();
+    });
+  }
+
+  // ✅ Función para calcular la presión arterial
+  function calcularPresion() {
+    const inputSistolica = document.getElementById("inputSistolica").value;
+    const inputDiastolica = document.getElementById("inputDiastolica").value;
+    const inputFecha = document.getElementById("inputFecha").value;
+
+    if (!inputDiastolica.trim() || !inputSistolica.trim() || !inputFecha.trim()) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Por favor, completa todos los campos.",
       });
+      return;
     }
-  
-    if (btnRitmoCardiaco) {
-      btnRitmoCardiaco.addEventListener("click", () => {
-        console.log("Ritmo Cardíaco seleccionado");
-      });
+
+    const systolic = parseInt(inputSistolica);
+    const diastolic = parseInt(inputDiastolica);
+    const status = getStatus({ systolic, diastolic });
+
+    // Mostramos el resultado
+    mostrarResultado(status);
+  }
+
+  // ✅ Función para obtener el estado de la presión arterial
+  function getStatus({ systolic, diastolic }) {
+    if (systolic < 60 || diastolic < 60) {
+      return "Low";
+    } else if ((systolic > 90 && systolic <= 120) || (diastolic >= 60 && diastolic <= 80)) {
+      return "Normal";
+    } else if ((systolic >= 121 && systolic <= 139) || (diastolic >= 80 && diastolic <= 89)) {
+      return "Risk";
+    } else if (systolic >= 140 || diastolic >= 90) {
+      return "High";
+    } else {
+      return "Invalid";
     }
-  
-    // Capturamos los datos del formulario de presión arterial
-    const formPresionArterial = document.getElementById("formPresionArterial");
-    if (formPresionArterial) {
-      formPresionArterial.addEventListener("submit", (event) => {
-        event.preventDefault();
-        calcularPresion();
-      });
+  }
+
+  // ✅ Función para mostrar el resultado
+  function mostrarResultado(status) {
+    switch (status) {
+      case "Low":
+        Swal.fire("Nivel de Presión Baja", "Recomendable ver a un médico.", "warning");
+        break;
+      case "Normal":
+        Swal.fire("Nivel de Presión Normal", "Sigue cuidándote.", "success");
+        break;
+      case "Risk":
+        Swal.fire("Presión en Riesgo", "Sugerencia visitar a un médico.", "warning");
+        break;
+      case "High":
+        Swal.fire("Niveles altos", "Visitar a tu médico lo antes posible.", "error");
+        break;
+      default:
+        Swal.fire("Error", "Datos inválidos, revisa tu medición.", "error");
+        break;
     }
+  }
+});
+
+// ✅ Función flecha para mostrar la información de la presión arterial
+const cargarInformacionPresion = () => {
+  const container = document.getElementById("presionInfoContainer");
   
-    // ✅ Función para calcular la presión arterial
-    function calcularPresion() {
-      const inputSistolica = document.getElementById("inputSistolica").value;
-      const inputDiastolica = document.getElementById("inputDiastolica").value;
-      const inputFecha = document.getElementById("inputFecha").value;
-  
-      if (!inputDiastolica.trim() || !inputSistolica.trim() || !inputFecha.trim()) {
-        Swal.fire({
-          icon: "error",
-          title: "Error",
-          text: "Por favor, completa todos los campos.",
-        });
-        return;
-      }
-  
-      const systolic = parseInt(inputSistolica);
-      const diastolic = parseInt(inputDiastolica);
-      const status = getStatus({ systolic, diastolic });
-  
-      // Mostramos el resultado
-      mostrarResultado(status);
-    }
-  
-    // ✅ Función para obtener el estado de la presión arterial
-    function getStatus({ systolic, diastolic }) {
-      if (systolic < 60 || diastolic < 60) {
-        return "Low";
-      } else if (
-        (systolic > 90 && systolic <= 120) ||
-        (diastolic >= 60 && diastolic <= 80)
-      ) {
-        return "Normal";
-      } else if (
-        (systolic >= 120 && systolic <= 139) ||
-        (diastolic >= 80 && diastolic <= 89)
-      ) {
-        return "Risk";
-      } else if (systolic >= 140 || diastolic >= 90) {
-        return "High";
-      } else {
-        return "Invalid";
-      }
-    }
-  
-    // ✅ Función para mostrar el resultado
-    function mostrarResultado(status) {
-      switch (status) {
-        case "Low":
-          Swal.fire("Nivel de Presión Baja", "Recomendable ver a un médico.", "warning");
-          break;
-        case "Normal":
-          Swal.fire("Nivel de Presión Normal", "Sigue cuidándote.", "success");
-          break;
-        case "Risk":
-          Swal.fire("Presión en Riesgo", "Sugerencia visitar a un médico.", "warning");
-          break;
-        case "High":
-          Swal.fire("Niveles altos", "Visitar a tu médico lo antes posible.", "error");
-          break;
-        default:
-          Swal.fire("Error", "Datos inválidos, revisa tu medición.", "error");
-          break;
-      }
-    }
-  });
+  // Verificamos si existe el contenedor
+  if (container) {
+    container.innerHTML = `
+      <div class="text-start space-y-4">
+        <p><b>${presionInfo.sistolica.titulo}:</b> ${presionInfo.sistolica.descripcion}</p>
+        <p><b>${presionInfo.diastolica.titulo}:</b> ${presionInfo.diastolica.descripcion}</p>
+      </div>
+    `;
+  }
+};
---
+++ utils/presionInfo/presionMockData.js
@@ -0,0 +1,12 @@
+export const presionInfo = {
+  sistolica: {
+    titulo: "Presión arterial Sistolica",
+    descripcion:
+      "Se refiere a la presión de la sangre en la arteria cuando se contrae el corazón. Es la cifra superior (y más alta) en una medición de la presión arterial.",
+  },
+  diastolica: {
+    titulo: "Presión arterial Diastolica",
+    descripcion:
+      "Se refiere a la presión de la sangre en la arteria cuando el corazón se relaja entre latidos. Es la cifra inferior (y más baja) en una medición de la presión arterial.",
+  },
+};
